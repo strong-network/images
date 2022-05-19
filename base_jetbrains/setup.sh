02f4f34c5e2cf0aedaa8d400037e7581e988493b
Bjorn Gudmundsson <bjorn@strong.network>
2022-05-19 13:16:24 +0000

    add add java intellij image

--- base_jetbrains/setup.sh
+++ base_jetbrains/setup.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env sh
+
 apt-get update 
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get install unzip openjdk-11-jdk -y
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get install procps -y
