5257e45fcc3eaba882ef422f97d732cb865d74ef
Bjorn Gudmundsson <bjorn@strong.network>
2022-10-05 11:10:08 +0000

    update to golang 1.19

--- generic/setup.sh
+++ generic/setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 
-wget -O golang_sdk-linux-amd64.tar.gz https://golang.org/dl/go1.18.linux-amd64.tar.gz
+wget -O golang_sdk-linux-amd64.tar.gz https://golang.org/dl/go1.19.linux-amd64.tar.gz
 tar -C /usr/local -xzf golang_sdk-linux-amd64.tar.gz
 rm golang_sdk-linux-amd64.tar.gz
 mv install_go_tools.sh /usr/bin/strong_network_startup/install_go_tools.sh
--- goland/setup.sh
+++ goland/setup.sh
@@ -1,7 +1,7 @@
 wget -O /ide.tar.gz https://download-cf.jetbrains.com/go/goland-2021.3.3.tar.gz
 /unzip_ide.sh
 
-wget -O golang_sdk-linux-amd64.tar.gz https://golang.org/dl/go1.18.linux-amd64.tar.gz
+wget -O golang_sdk-linux-amd64.tar.gz https://golang.org/dl/go1.19.linux-amd64.tar.gz
 
 tar -C /usr/local -xzf golang_sdk-linux-amd64.tar.gz
 rm golang_sdk-linux-amd64.tar.gz
