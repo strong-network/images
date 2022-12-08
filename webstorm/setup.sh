da5de69e7dc57e4847b342274224174fabea62ec
Bjorn Gudmundsson <bjorn@strong.network>
2022-12-08 09:35:07 +0000

    fix

--- webstorm/setup.sh
+++ webstorm/setup.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env sh
 
-# Add nodejs 16 repository
+set -e
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+rm packages-microsoft-prod.deb
+
+wget -O /ide.tar.gz https://download.jetbrains.com/webstorm/WebStorm-2022.2.3.tar.gz
+/unzip_ide.sh
+
+# Add nodejs 18 repository
 curl -fsSL https://deb.nodesource.com/setup_18.x | bash -
 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
 
@@ -15,15 +23,7 @@ export NVM_DIR="$HOME/.nvm"
 nvm install 18.10.0
 
 nvm use 18.10.0
-
-wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-
-apt-get update && apt-get install -y dotnet-sdk-7.0
-
-wget -O /ide.tar.gz https://download.jetbrains.com/webstorm/WebStorm-2022.2.3.tar.gz
-/unzip_ide.sh
+apt-get update && apt-get -y install gcc g++ make yarn dotnet-sdk-7.0
 
 DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true apt-get install php libapache2-mod-php -y
 
